8db649a94f623e955453ab685553d96c950b20a4
Max Kaye <m@xk.io>
2013-11-08 16:13:08 +1100

    Membership admin features 	examine and modify tiers and payments 	cleaned up (in progress) Template now supported (for form) database design updates

--- membershipAdmin.py
+++ membershipAdmin.py
@@ -5,49 +5,237 @@ from pycoin.wallet import Wallet
 import redis
 r = redis.StrictRedis(host='localhost', port=6379, db=0)
 
-## FUNCITONS - MAP TO COMMANDS
+# change these
 
-def printHelp():
-	print """Commands: 
-	exit, help, printAllMembers
-	"""
+orgName = 'bitcoinAustralia'
+
+# debug or config vars
+
+printRaw = False
+
+# first entry in list should be the short human identifier (like a name)
+dbmap = {
+	"tiers":['shortName','description','cost','duration','founding','suggestedSize'],
+	"members":['name','active','activeFrom','activeFor','email','founding','paymentAddress'],
+	"payments":['description','amount','date','paid']
+}
+
+## FUNCITONS - MAP TO COMMANDS
 
 def endProgram():
 	sys.exit()
+	
+def confirmWrite(loc, val):
+	ans = ''
+	while ans != 'y':
+		print 'Warning!'
+		print 'About to set the following:'
+		print '%s -> %s' % (loc, val)
+		ans = raw_input(' Confirm OKAY (press <y>); Ctrl-c or <n> to end > ')
+		if ans == 'n':
+			return False
+	return True
+	
 
-def getAllMembers():
-	lastMember = int(r.get('bitcoinAustralia:lastmemberid'))
-	toRet = []
+def getMembers(activeTest=False):
+	lastMember = r.get('%s:members:counter' % orgName)
+	lastMember = int(lastMember)
+	toRet = [] 
+	toRet += [['id']+dbmap['members']]
 	for i in range(lastMember):
-		toAdd = []
-		for info in ['name','email','resAddress','paymentAddress']:
-			toAdd += [r.get('bitcoinAustralia:members:%d:%s' % (i+1, info))]
+		toAdd = [i+1]
+		active = r.get('%s:members:%d:%s' % (orgName, i+1, 'active'))
+		if activeTest:
+			if active != 'true':
+				continue
+		for info in dbmap['members']:
+			toAdd += [r.get('%s:members:%d:%s' % (orgName, i+1, info))]
 		toRet += [toAdd]
 	return toRet
 	
-def printAllMembers():
-	allMembers = getAllMembers()
-	for row in allMembers:
-		for thing in row:
-			print thing,
-		print
-		
+def getActiveMembers():
+	return getMembers(activeTest=True)
 
+
+	
+def getTiers():
+	totalTiers = r.get('%s:tiers:counter' % orgName)
+	if totalTiers == None:
+		return None
+	totalTiers = int(totalTiers)
+	toRet = []
+	fields = dbmap['tiers']
+	toRet += [['id']+fields]
+	for i in range(totalTiers):
+		toAdd = [i+1]
+		for f in fields:
+			toAdd += [r.get('%s:tiers:%d:%s' % (orgName, i+1, f))]
+		toRet += [toAdd]
+	return toRet
+		
+		
+def getPayments(memberid):
+	memberid = str(memberid)
+	totalPayments = r.get('%s:members:%s:payments:counter' % (orgName, memberid))
+	if totalPayments == None:
+		return None
+	totalPayments = int(totalPayments)
+	toRet = []
+	fields = dbmap['payments']
+	toRet += [['id']+fields]
+	for i in range(totalPayments):
+		toAdd = [i+1]
+		for f in fields:
+			toAdd += [r.get('%s:members:%s:payments:%d:%s' % (orgName, memberid, i+1, f))]
+		toRet += [toAdd]
+	return toRet
+	
+def printGeneric(name, toPrint):
+	allItems = toPrint
+	if allItems == None:
+		print 'No %s; try add%s' % (name, name[:-1].capitalize())
+		return
+	for row in allItems:
+		for item in row:
+			if not printRaw:
+				print '%10s |' % str(item)[:10],
+			else:
+				print '%s |' % str(item),
+		print ''
+			
+def printTiers():
+	printGeneric('tiers',getTiers())
+	
+def printMembers(activeTest=False):
+	printGeneric('members',getMembers(activeTest=activeTest))
+		
+def printActiveMembers():
+	printMembers(activeTest=True)
+	
+def printPayments():
+	memberid = raw_input('Payments of what Member ID? > ')
+	printGeneric('payments',getPayments(memberid))
+		
+def numGeneric(itemType):
+	return int(r.get('%s:%s:counter' % (orgName, itemType)))
+		
+def numTiers():
+	return numGeneric('tiers')
+	
+def numMembers():
+	return numGeneric('members')
+	
+def addTier():
+	details = {}
+	details['shortName'] = raw_input(' Short Name > ')
+	details['description'] = raw_input(' Description > ')
+	details['cost'] = raw_input(' Cost (in BTC) > ')
+	details['duration'] = raw_input(' Duration (in seconds) > ')
+	details['founding'] = ''
+	while details['founding'] not in ['true','false']:
+		details['founding'] = raw_input(' Is a founding member? (\'true\' or \'false\') > ')
+	details['suggestedSize'] = raw_input(' Suggested Size (for organisations) > ')
+	
+	tierID = r.incr('%s:tiers:counter' % orgName)
+	for k,v in details.iteritems():
+		r.set('%s:tiers:%s:%s' % (orgName, tierID, k), v)
+	r.set('%s:tiers:shortNameToID:%s' % (orgName, details['shortName']), tierID)
+	r.rpush('%s:tiers:shortNameList' % orgName, details['shortName'])
+	print 'Created tier %s' % tierID
+	
+def modGeneric(itemType):
+	if itemType == 'tiers':
+		itemName = 'Tiers'
+	elif itemType == 'members':
+		itemName = 'Members'
+	else:
+		itemName = itemType
+		
+	nameField = dbmap[itemType][0]
+	for i in range(numGeneric(itemType)):
+		t = r.get('%s:%s:%d:%s' % (orgName, itemType, i+1, nameField))
+		print '%s:%s:%d:%s' % (orgName, itemType, i+1, nameField)
+		print '    %s. %s' % (i+1, t)
+		
+	idToMod = raw_input(' ID To Modify > ')
+	
+	print 'Fields:',
+	for f in dbmap[itemType]:
+		print f,
+	print ''
+		
+	inputField = ''
+	while inputField not in dbmap[itemType]:
+		inputField = raw_input(' Field to modify > ')
+		
+	newValue = raw_input(' New Value > ')
+	dbLocation = '%s:%s:%s:%s' % (orgName, itemType, idToMod, inputField)
+	if not confirmWrite(dbLocation, newValue):
+		print 'No write will happen - bailing out'
+		return False
+	r.set(dbLocation, newValue)
+	
+	print 'Generic Done'
+	return (itemType,idToMod,inputField,newValue)
+		
+	
+def modTier():
+	itemType,idToMod,field,newValue = modGeneric('tiers')
+	
+	if field == 'shortName':
+		r.set('%s:tiers:shortNameToID:%s' % (orgName, newValue), tierID)
+		r.rpush('%s:tiers:shortNameList' % orgName, newValue)
+	
+	print 'Custom Done'
+	
+def modMember():
+	itemType,idToMod,field,newValue = modGeneric('members')
+	
+	if field == 'email':
+		r.set('%s:members:emailHashToID:%s' % (orgName, sha256Hash(newValue)), idToMod)
+		
+	print 'Custom Done'
+	
+## HELP AND HELPERS
+
+def printHelp():
+	print """Commands: """
+	for i in functionMap.keys():
+		print '%s,' % i,
+	print ''
+
+functionMap = {
+	"printMembers":printMembers,
+	"printActiveMembers":printActiveMembers,
+	"printTiers":printTiers,
+	"printPayments":printPayments,
+	"help":printHelp,
+	"exit":endProgram,
+	"addTier":addTier,
+	"modTier":modTier,
+}
 
 ## MAIN - RUN APP
 
-functionMap = {
-	"printAllMembers":printAllMembers,
-	"help":printHelp,
-	"exit":endProgram,
-}
+def exCommand(command):
+	if command not in functionMap.keys():
+		print 'Error: command nonexistant.'
+	else:
+		functionMap[command]()
+	
 
 if __name__ == "__main__":
+	if len(sys.argv) > 1:
+		if '-raw' in sys.argv:
+			global printRaw 
+			printRaw = True
+			sys.argv.remove('-raw')
+		command = sys.argv[1]
+		exCommand(command)
+		sys.exit()
 	while True:
 		command = raw_input('$> ')
 		if command not in functionMap.keys():
-			print 'Error: command nonexistant.'
-		else:
-			functionMap[command]()
+			exCommand(command)
 	
 	
--- signup.py
+++ signup.py
@@ -12,7 +12,7 @@ from pycoin.wallet import Wallet
 pubWallet = Wallet.from_wallet_key(pubKey)
 
 from flask import Flask
-from flask import request
+from flask import request, render_template
 app = Flask(__name__)
 
 import logging
@@ -21,6 +21,7 @@ log_handler.setLevel(logging.WARNING)
 app.logger.addHandler(log_handler)
 
 from Crypto.Hash import SHA256
+import time
 
 # DEBUG STUFF FOR PAYMENTS
 """
@@ -40,131 +41,6 @@ pubWallet = Wallet.from_wallet_key(pubKey)
 
 # this is used in the redis db
 orgName = 'bitcoinAustralia'
-
-## HTML
-
-# this will eventually be a customizable template
-form_html = """
-	<!--<script src="https://code.jquery.com/jquery-1.10.1.min.js"></script>-->
-	<!--<script src="http://127.0.0.1/jquery-1.10.1.min.js"></script>-->
-	
-						<div class="row">
-							<div class="12u">
-							</div>
-						</div>
-	<div id="memberForm" class="row half">
-		<div class="12u">
-			<form role="form" id="membershipForm">
-				<div class="row half">
-					<div class="6u">
-						<input type="text" class="form-control" id="memberName" name="memberName" placeholder="Name">
-					</div>
-					<div class="6u">
-						<input type="text" class="form-control" id="memberEmail" name="memberEmail" placeholder="Email">
-					</div>
-				</div>
-				<div class="row half">
-					<div class="12u">
-						<textarea rows="5" maxlength="512" id="memberAddress" name="memberAddress" placeholder="Residential Address"></textarea>
-					</div>
-				</div>
-				<div class="row half">
-					<div class="12u">
-						<label><input type="checkbox" id="memberAllowed" name="memberAllowed"> I am a citizen or permenant resident of Australia, OR, an incorporated or registered legal entitiy according to the relevant Australian state or federal law.</label>
-					</div>
-				</div>
-				<div class="row">
-					<div class="12u">
-						<a class="button" id="memberSubmit">Get Payment Address</a>
-						<a class="button button-alt form-button-reset" onclick="$('#membershipForm')[0].reset();">Clear Form</a>
-					</div>
-				</div>
-			</form>
-		</div>
-	</div>
-	<div class="row half" id="loadingPayment" style="display:none;">
-		<div class="12u">
-			<h3>Loading<span id="loadingAnimation"></span></h3>
-		</div>
-	</div>
-	<div class="row half" id="memberPaymentAddress" style="display:none;">
-		<div class="12u">
-			Please pay <span id="paymentAmount"></span> to:
-			<h3 id="addressToPay"></h3>
-			<a id="bitcoinURI" href=""><img id="bitcoinQR" src="" alt="QR code and bitcoin: link" width="300" height="300"></a>
-			
-			<p>Once you've sent payment there is nothing more you are required to do. Your membership will be manually processed and you will be emailed when it is complete.
-			<br><br>
-			Kind Regards,<br>Bitcoin Australia</p>
-			<!-- THIS IS DEBUG STUFF -->
-			<!-- <div class="row">
-				<div class="12u">
-					<a class="button" id="paymentGoBack">Back</a>
-				</div>
-			</div> -->
-		</div>
-	</div>
-	<div class="row half" id="memberError" style="display:none;">
-		<div class="12u">
-			<h3 class="error">Error: <span id="errorReport"></span></h3>
-			<div class="row">
-				<div class="12u">
-					<a class="button" id="errorGoBack">Back</a>
-				</div>
-			</div>
-		</div>
-	</div>
-	<script type="text/javascript">
-	$("#memberSubmit").click(function(){
-		$("#memberForm").slideUp(200);
-		$("#loadingPayment").slideDown(200);
-		$.ajax({
-			"success":function(data, z, y){
-				console.log(data);
-				paymentDetails = JSON.parse(data);
-				if(paymentDetails["error"] == "none"){
-					$("#paymentAmount").html(paymentDetails["amount"]);
-					$("#addressToPay").html(paymentDetails["address"]);
-					$("#bitcoinURI").attr("href",paymentDetails["uri"]);
-					$("#bitcoinQR").attr("src","https://chart.googleapis.com/chart?cht=qr&chs=300x300&chl="+encodeURIComponent(paymentDetails["uri"]));
-					$("#loadingPayment").slideUp(200);
-					$("#memberPaymentAddress").slideDown(200);
-				}else{
-					$("#errorReport").html(paymentDetails["error"]);
-					$("#loadingPayment").slideUp(200);
-					$("#memberError").slideDown(200);
-				}
-			},
-			"type":"POST",
-			"url":"/membership",
-			"data":{
-				"memberEmail":$("#memberEmail").val(),
-				"memberName":$("#memberName").val(),
-				"memberAddress":$("#memberAddress").val(),
-				"memberAllowed":$("#memberAllowed").is(":checked")
-			},
-		});
-	});
-	$("#errorGoBack").click(function(){
-		$("#memberError").slideUp(200);
-		$("#memberForm").slideDown(200);
-	});
-	$("#paymentGoBack").click(function(){
-		$("#memberPaymentAddress").slideUp(200);
-		$("#memberForm").slideDown(200);
-	});
-	
-	loadingAnimCounter = 0;
-	loadingAnimation = function(){
-		if($("#loadingPayment").is(":visible")){
-			loadingAnimCounter += 1;
-			$("#loadingAnimation").html(["",".","..","..."][loadingAnimCounter % 4]);
-		}
-		setTimeout(loadingAnimation,500);
-	}
-	loadingAnimation();
-	</script>
-	"""
 	
 ## FUNCTIONS
 
@@ -192,7 +68,7 @@ class Database:
 	def checkEmailExists(self, email):
 		return self.r.exists('%s:members:emailHashToId:%s' % (self.orgName, sha256Hash(email)))
 	def getNewMemberNumber(self): # increment usercounter and get new member number
-		return self.r.incr('bitcoinAustralia:lastmemberid')
+		return self.r.incr('%s:members:counter' % self.orgName)
 	def setUserDetails(self,details): # details is a dict of keyvalue pairs to set - keep at arms length from db
 		'''details should be a dict with keys 'resAddress','name','email','id' '''
 		memberid = int(details['id'])
@@ -203,33 +79,37 @@ class Database:
 		self.r.set('%s:members:%d:resAddress' % (self.orgName, memberid), resAddress)
 		self.r.set('%s:members:%d:name' % (self.orgName, memberid), name)
 		self.r.set('%s:members:%d:email' % (self.orgName, memberid), email)
-		self.r.set('%s:members:%d:paid' % (self.orgName, memberid), 'false')
 		self.r.set('%s:members:%d:paymentAddress' % (self.orgName, memberid), getPaymentAddress(memberid))
+		self.r.set('%s:members:%d:active' % (self.orgName, memberid), 'false')
 		
-		self.r.sadd('%s:uncertainMembersSet' % self.orgName, memberid)
 		self.r.set('%s:members:emailHashToId:%s' % (self.orgName, sha256Hash(email)), memberid)
-	def getIndividualFeeYearly(self):
-		fee = self.r.get('%s:membership:feeIndividualYearly' % self.orgName)
+	def getFee(self, tierID):
+		fee = self.r.get('%s:tiers:%s:cost' % (self.orgName, str(tierID)))
 		return fee
-	def setIndividualFeeYearly(self, fee):
-		fee = str(fee)
-		return self.r.set('%s:membership:feeIndividualYearly' % self.orgName, fee)
+	def addPaymentRequest(self,memberid,amount,description,date):
+		prefix = '%s:members:%d:payments' % (self.orgName, int(memberid))
+		newPaymentID = self.r.incr('%s:counter' % prefix)
+		prefix += ':%s' % newPaymentID
+		self.r.set('%s:amount' % prefix, amount)
+		self.r.set('%s:description' % prefix, description)
+		self.r.set('%s:date' % prefix, date)
+		self.r.set('%s:paid' % prefix, 'false')
 
 ## ROUTES (PAGES)
 
 @app.route("/membership", methods=["POST","GET"])
 def membership():
 	if request.method == 'GET':
-		global form_html
-		return form_html
+		return render_template('form.html')
 	elif request.method == 'POST':
 		try:	
 			email = request.form['memberEmail']
 			name = request.form['memberName']
 			resAddress = request.form['memberAddress']
 			allowed = request.form['memberAllowed']
+			tier = int(request.form['memberTier'])
 		except:
-			return '{"error":"Submitted fields incorrect"}'
+			return '{"error":"Submitted fields incorrect; general exception"}'
 		if '' in [email, name, resAddress]:
 			return '{"error":"One of name, email or address is blank"}'
 		if db.checkEmailExists(email):
@@ -246,9 +126,12 @@ def membership():
 			'email':email
 		})
 		address = getPaymentAddress(memberid)
-		amount = db.getIndividualFeeYearly()
+		amount = db.getFee(tier)
 		comment = "Bitcoin%20Australia%20Membership"
 		uri = "bitcoin:%s?amount=%s&label=%s" % (address, amount, comment)
+		
+		db.addPaymentRequest(memberid,amount,'Membership 365 days, tier %s' % str(tier),int(time.time()))
+		
 		return '{"address":"%s","amount":"%s","uri":"%s","error":"none"}' % (address, amount, uri)
 		
 @app.route("/memberlist")
